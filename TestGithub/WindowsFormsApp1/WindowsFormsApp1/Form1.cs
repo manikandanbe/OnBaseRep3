bfaa013749277e37741e7d4a700641f4ffc60a50
manikandanbe <132265135+manikandanbe@users.noreply.github.com>
2023-05-02 18:35:54 +0400

    message box added

--- TestGithub/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ TestGithub/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -18,7 +18,8 @@ namespace WindowsFormsApp1
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {s
+        {
+            MessageBox.Show("test");
         }
     }
 }
